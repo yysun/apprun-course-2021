9ce12f6305e74cf4128a94100f615f2e52645030
Yiyi Sun <yiyisun@gmail.com>
2021-01-01 11:29:19 -0500

    open aside from note page

--- ch5/L1-github/src/pages/Notes/NotesAside.tsx
+++ ch5/L1-github/src/pages/Notes/NotesAside.tsx
@@ -7,8 +7,7 @@ export default class NotesAsideComponent extends Component {
   view = ({ article, notes }) => article
     ?
     <div class="row">
-
-      <div class="list-group list-group-accent">
+      <div class="list-group list-group-accent w-100">
         <div class="list-group-item list-group-item-accent-dark bg-light text-center text-muted c-small">{article.title}</div>
         {notes.map((note, idx) => <div class="list-group-item list-group-item-accent-secondary c-small"
           contenteditable="plaintext-only" $oninput={['edit', idx]} >
@@ -27,11 +26,17 @@ export default class NotesAsideComponent extends Component {
   update = {
     '/display-article': (state, article) => {
       if (article) {
-        app.run('show-aside');
+        // app.run('show-aside');
         state.article = article;
         const { notes } = api.note(state.article.id) || {};
         state.notes = notes ?? [];
         return state;
+      }
+    },
+    '/display-note': (state, article, notes) => {
+      if (article) {
+        app.run('show-aside');
+        return { article, notes };
       }
     },
     add: (state) => {
@@ -43,7 +48,7 @@ export default class NotesAsideComponent extends Component {
       state.notes[idx] = text;
       const { path, title, cover_image } = state.article;
       api.save(state.article.id, {
-        article: { path, title, cover_image },
+        article: { id: state.article.id, path, title, cover_image },
         notes: state.notes
       });
       return state;
