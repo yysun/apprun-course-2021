a9db07aae4a092af05e81d27385ba61d05143f9e
Yiyi Sun <yiyisun@gmail.com>
2021-01-01 11:30:42 -0500

    add event for creating gist

--- ch5/L1-github/src/pages/Notes/index.tsx
+++ ch5/L1-github/src/pages/Notes/index.tsx
@@ -1,16 +1,26 @@
 import { app, Component } from 'apprun';
 import api from './NotesApi';
+import GistApi from '../Gist/GistApi';
+
+const gist_link = id => `https://gist.github.com/${id}`;
 
 const Card = ({ article, notes }) => <div class="col-12 col-lg-4 col-md-6 col-sm-12 mb-2">
-  <div class="card h-100 w-100 d-inline-block">
+  <div class="card h-100 w-100 d-inline-block d-flex flex-column">
     {article.cover_image && <img class="card-img-top" src={article.cover_image} alt="" />}
     <div class="card-body">
-      <h5 class="card-title text-muted">{article.title}</h5>
-      {notes.map(note => <p>{ note }</p>)}
-      <div class="text-right">
-        <a href='#' $onclick='gist' class="card-link"><i class="fa fa-github"/> Gist</a>
-        <a href={`#Article${article.path}`} class="card-link">Read...</a>
+      <a href={`#Article${article.path}`}>
+        <h5 class="card-title">{article.title}</h5>
+      </a>
+      <div style="cursor: pointer" $onclick={['/display-note', article, notes]}>
+        {notes.map(note => <p>{note}</p>)}
       </div>
+    </div>
+    <div class="mt-auto m-3 text-right">
+      {article.gist_id
+        ? <a href={gist_link(article.gist_id)} target="_blank" class="card-link"><i class="fa fa-github" /></a>
+        : <a href='#' $onclick={['add-gist', article, notes]} class="card-link"><i class="fa fa-plus"/> Gist</a>
+      }
+      <a href={`#Article${article.path}`} target="_blank" class="card-link">Read...</a>
     </div>
   </div>
 </div>;
@@ -24,5 +34,35 @@ export default class NotesComponent extends Component {
 
   update = {
     '#Notes': () => api.notes(),
+    'add-gist': async (state, article, notes, e) => {
+      e.preventDefault();
+      try {
+        const my_notes = notes.join('\n');
+        const content = `[${article.title}](https://dev.to/${article.path})\n\n${my_notes}`;
+        let gist = {
+          description: 'RE ' + article.title,
+          'files': {
+            'index.md': {
+              'filename': 'README.md',
+              language: 'Markdown',
+              type: 'text/markdown',
+              content
+            }
+          },
+          'public': false
+        };
+        gist = await GistApi.newGist(gist);
+        article.gist_id = gist['id'];
+        api.save(article.id, { article, notes });
+        app.run('@show-modal', {
+          title: 'Created a gist',
+          body: `_html:<a href="${gist_link(article.gist_id)}" target="_blank">
+            ${gist_link(article.gist_id)}
+          </a>` });
+        return state;
+      } catch (ex) {
+        app.run('@show-modal', { title: 'Unable to create gist', body: ex });
+      }
+    }
   };
 }
