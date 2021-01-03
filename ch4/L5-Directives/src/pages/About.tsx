d67493d4fac78a30ac4564fa7c9f472a390ba1dd
Yiyi Sun <yiyisun@gmail.com>
2021-01-03 00:58:25 -0500

    add examples of $onclick

--- ch4/L5-Directives/src/pages/About.tsx
+++ ch4/L5-Directives/src/pages/About.tsx
@@ -1,27 +1,34 @@
 import { app, Component } from 'apprun';
 
 const add = (state, num) => state + num;
+const dec = state => state - 1;
+const inc = state => state + 1;
+
+const counter_view = state => <>
+  <h1>{state}</h1>
+  <button class="btn btn-light" $onclick='-1'>-1</button>&nbsp;
+  <button class="btn btn-light" $onclick='+1'>+1</button> call event names
+  <hr />
+  <button class="btn btn-light" $onclick={['add', -1]}>-1</button>&nbsp;
+  <button class="btn btn-light" $onclick={['add', +1]}>+1</button> call events with parameters
+  <hr />
+  <button class="btn btn-light" $onclick={dec}>-1</button>&nbsp;
+  <button class="btn btn-light" $onclick={inc}>+1</button> call action functions
+  <hr />
+  <button class="btn btn-light" $onclick={[add, -1]}>-1</button>&nbsp;
+  <button class="btn btn-light" $onclick={[add, +1]}>+1</button> call action functions with parameters
+</>;
 
 export default class AboutComponent extends Component {
 
   state = 0;
 
-  view = state => <>
-    <h1>{state}</h1>
-    <button class="btn btn-light" $onclick='-1'>-1</button>&nbsp;
-    <button class="btn btn-light" $onclick='+1'>+1</button> call event names
-    <hr />
-    <button class="btn btn-light" $onclick={['add', -1]}>-1</button>&nbsp;
-    <button class="btn btn-light" $onclick={['add', +1]}>+1</button> call events with parameters
-    <hr />
-    <button class="btn btn-light" $onclick={[add, -1]}>-1</button>&nbsp;
-    <button class="btn btn-light" $onclick={[add, +1]}>+1</button> call action functions
-  </>;
+  view = counter_view;
 
   update = {
     '#About': state => state,
-    '-1': state => state - 1,
-    '+1': state => state + 1,
+    '-1': dec,
+    '+1': inc,
     add
   };
 }
